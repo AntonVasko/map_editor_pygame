dfa9deec0adb750959cdcf9db2164a3c916f7c76
AntonVasko <vasko.anton.a@gmail.com>
2025-07-11 00:01:10 +0300

    move Rect

--- main.py
+++ main.py
@@ -20,6 +20,10 @@ resize = False
 color = 'green'
 other_rects = {}
 press_ctrl = False
+press_left = False
+press_right = False
+press_up = False
+press_down = False
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -29,7 +33,29 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN and event.key == pygame.K_LCTRL:
             click = True
-            
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+            press_left = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+            press_right = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+            press_up = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+            press_down = True    
+        
+        if event.type == pygame.KEYUP and event.key == pygame.K_LEFT:
+            press_left = False
+            other_rects.clear()
+        if event.type == pygame.KEYUP and event.key == pygame.K_RIGHT:
+            press_right = False
+            other_rects.clear()
+        if event.type == pygame.KEYUP and event.key == pygame.K_UP:
+            press_up = False
+            other_rects.clear()
+        if event.type == pygame.KEYUP and event.key == pygame.K_DOWN:
+            press_down = False
+            other_rects.clear()
+
         if event.type == pygame.KEYUP and event.key == pygame.K_LCTRL:
             click = False
             press_ctrl = False
@@ -59,24 +85,25 @@ while running:
             print(x_pos, y_pos)
             x_old = x_pos
             y_old = y_pos
+            
 
             for rect in rects:
-                if rect.collidepoint(x_pos, y_pos) and not click:
+                if rect.collidepoint(x_pos, y_pos) and not click and resize:
                     if y_new > y_old:
                         y_new = y_pos
                         rect.height += 1
                     print("Курсор внутри прямоугольника!")
-                if rect.collidepoint(x_pos, y_pos) and not click:
+                if rect.collidepoint(x_pos, y_pos) and not click and resize:
                     if y_new < y_old:
                         y_new = y_pos
                         rect.height -= 1
                     print("Курсор внутри прямоугольника!")
-                if rect.collidepoint(x_pos, y_pos) and not click:
+                if rect.collidepoint(x_pos, y_pos) and not click and resize:
                     if x_new < x_old:
                         x_new = x_pos
                         rect.width += 1
                     print("Курсор внутри прямоугольника!")
-                if rect.collidepoint(x_pos, y_pos) and not click:
+                if rect.collidepoint(x_pos, y_pos) and not click and resize:
                     if x_new > x_old:
                         x_new = x_pos
                         rect.width -= 1
@@ -87,41 +114,55 @@ while running:
                     rect.centery = y_pos
                     other_rects[rects.index(rect)] = rect
                     press_ctrl = True
-                    
                     print('Поймал', other_rects)
+                
+
                 #other_rects = {}
         
 
     # fill the screen with a color to wipe away anything from last frame
     
-    rects_H.draw(screen)
-    
-    for rect in rects:
-        if rect.collidepoint(x_rem, y_rem):
-            rects.remove(rect)
-        if len(other_rects) != 0:
-            for other in other_rects:
-                print(other)
-                if (other_rects[other].colliderect(rect) and click and press_ctrl):
-                    color = 'red'
-                    print(color)
-                else:
-                    color = 'green'
-        else:
-            color = 'green'
-            #pygame.draw.rect(screen, color, rect)
-        
-        '''collision_rect = rect.collidelist(rects)
-        if collision_rect != -1:
-            print('Пересекаюсь', collision_rect)'''
-
-        '''for rect_any in range(1, len(rects)):
-            if rects[rect_any].colliderect(rects[rect_any-1]):
-                color = 'red'
+    #rects_H.draw(screen)
+    if len(rects) != 0:
+        for rect in rects:
+            if rect.collidepoint(x_rem, y_rem):
+                rects.remove(rect)
+            if rect.collidepoint(pygame.mouse.get_pos()) and press_left and not click:
+                other_rects[rects.index(rect)] = rect
+                rect.x -= 1
+            if rect.collidepoint(pygame.mouse.get_pos()) and press_right and not click:
+                other_rects[rects.index(rect)] = rect
+                rect.x += 1
+            if rect.collidepoint(pygame.mouse.get_pos()) and press_up and not click:
+                other_rects[rects.index(rect)] = rect
+                rect.y -= 1
+            if rect.collidepoint(pygame.mouse.get_pos()) and press_down and not click:
+                other_rects[rects.index(rect)] = rect
+                rect.y += 1
+            
+            if len(other_rects) != 0:
+                for other in other_rects:
+                    print(other)
+                    if (other_rects[other].colliderect(rect) and click and press_ctrl) or (other_rects[other].colliderect(rect) and (press_down or press_right or press_left or press_up)):
+                        color = 'red'
+                        print(color)
+                    else:
+                        color = 'green'
             else:
                 color = 'green'
-            pygame.draw.rect(screen, color, rects[rect_any-1])'''
-        pygame.draw.rect(screen, color, rect)
+                #pygame.draw.rect(screen, color, rect)
+            
+            '''collision_rect = rect.collidelist(rects)
+            if collision_rect != -1:
+                print('Пересекаюсь', collision_rect)'''
+
+            '''for rect_any in range(1, len(rects)):
+                if rects[rect_any].colliderect(rects[rect_any-1]):
+                    color = 'red'
+                else:
+                    color = 'green'
+                pygame.draw.rect(screen, color, rects[rect_any-1])'''
+            pygame.draw.rect(screen, color, rect)
 
     # RENDER YOUR GAME HERE
 
